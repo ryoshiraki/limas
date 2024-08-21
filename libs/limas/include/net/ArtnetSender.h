f0140928b9b552ec9bfcad07587a0636c2e51d42
ryoshiraki <shirakiryo@gmaill.com>
2024-08-22 03:37:10 +0900

    fix artnet sender

--- libs/limas/include/net/ArtnetSender.h
+++ libs/limas/include/net/ArtnetSender.h
@@ -9,30 +9,52 @@ namespace limas {
 namespace net {
 class ArtnetSender {
  public:
-  ~ArtnetSender() { stop(); }
-
-  void start(boost::asio::io_service& io_service, const std::string& ip,
-             int port = 6454) {}
+  void setup(boost::asio::io_service& io_service, const std::string& ip,
+             int port = 6454) {
+    client_ = std::make_shared<UdpClient>(io_service, ip, port);
+    dmx_.fill(0);
+  }
 
   void set(uint16_t ch, uint8_t val) {
-    auto locker = getLock();
     if ((ch - 1) < 0 || (ch - 1) > dmx_.size()) {
-      throw rs::Exception("Invalid channel range specified.");
+      throw limas::Exception("Invalid channel range specified.");
     }
     dmx_[ch - 1] = val;
   }
 
+  uint8_t getValue(uint16_t ch) {
+    if ((ch - 1) < 0 || (ch - 1) > dmx_.size()) {
+      throw limas::Exception("Invalid channel range specified.");
+    }
+    return dmx_[ch - 1];
+  }
+
+  void send() {
+    std::array<uint8_t, 530> packet;  // Art-Net header + DMX data (512 bytes)
+    std::fill(packet.begin(), packet.end(), 0);
+
+    // Art-Net Header
+    std::string header = "Art-Net";
+    std::copy(header.begin(), header.end(), packet.begin());
+    packet[8] = 0x00;  // OpCode (ArtDMX)
+    packet[9] = 0x50;
+    packet[10] = 0x0E;  // Protocol version
+    packet[11] = 0x00;
+    packet[12] = 0x00;                       // Sequence
+    packet[13] = 0x00;                       // Physical
+    packet[14] = 0x00;                       // Universe (low byte)
+    packet[15] = 0x00;                       // Universe (high byte)
+    packet[16] = (dmx_.size() >> 8) & 0xFF;  // Data length high byte
+    packet[17] = dmx_.size() & 0xFF;         // Data length low byte
+
+    std::copy(dmx_.begin(), dmx_.end(), packet.begin() + 18);
+    client_->send(std::vector<unsigned char>(packet.begin(), packet.end()));
+  }
+
  protected:
   boost::asio::io_service* io_service_;
-  net::UdpClient::Ptr client_;
+  std::shared_ptr<net::UdpClient> client_;
   std::array<uint8_t, 512> dmx_;
-
- private:
-  void threadedFunction() {
-    io_service_->run();
-    while (isThreadRunning()) {
-    }
-  }
 };
 
 }  // namespace net
